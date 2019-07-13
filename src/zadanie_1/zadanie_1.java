7565ef86e47df6e04d8a329686658ab4caf54f23
Marcin Klauzinski <marcin.klauzinski@gmail.com>
2019-07-13 13:43:30 +0200

    poprawka

---
+++ src/zadanie_1/zadanie_1.java
@@ -0,0 +1,16 @@
+package zadanie_1;
+
+import java.util.Random;
+
+public class zadanie_1 {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+
+
+        System.out.println("Gra w zgadywanie liczb");
+
+            }
+        }
+    }
+}
